2e8fb823238a2367bc937a9821ecbd3643d28f21
Methuni Mallikaratchy <methuni.20230435@iit.ac.lk>
2024-12-02 23:02:26 +0530

    corrected a misplaced code snippet

--- src/main/java/Classes/Vendor.java
+++ src/main/java/Classes/Vendor.java
@@ -5,30 +5,32 @@ public class Vendor implements Runnable{
     private final TicketPool ticketPool;
     public final int releaseRate;
 
+
     public Vendor(String vendorID, TicketPool ticketPool, int releaseRate){
         this.vendorID=vendorID;
-        this.ticketPool=ticketPool;
+        this.ticketPool= ticketPool;
         this.releaseRate=releaseRate;
     }
     @Override
     public void run(){
         try {
-            String[] tickets = null;
             while (!Thread.currentThread().isInterrupted()) {
-                tickets = new String[releaseRate];
+                String[] tickets = new String[releaseRate];
                 for (int i = 0; i < releaseRate; i++) {
                     tickets[i] = "Ticket - " + vendorID + " - " + System.currentTimeMillis() + " - " + i;
 
                 }
+                ticketPool.addTickets(tickets);
+                System.out.println("Vendor " + vendorID + " added " + releaseRate + " tickets.");
+                Thread.sleep(1000);
             }
-            ticketPool.addTickets(tickets);
-            System.out.println("Vendor " + vendorID + " added " + releaseRate + " tickets.");
-            Thread.sleep(1000);
         }catch(InterruptedException e){
             System.out.println("Vendor "+vendorID+" interrupted.");
+            Thread.currentThread().interrupt();
         }catch(Exception e){
             System.out.println("Vendor "+vendorID+" error: "+e.getMessage());
         }
 
     }
+
 }
